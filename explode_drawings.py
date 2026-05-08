ec2649d4a5d93d472e6afc37ff3fe84fe83a3e31
Дмитрий <kuvbur@gmail.com>
2026-05-08 19:12:24 +0300

    Update explode_drawings.py

--- explode_drawings.py
+++ explode_drawings.py
@@ -11,47 +11,55 @@ def get_navigator_item_guids_from_tree(current_branch, navigator_item_guids, nav
         if children:
             get_navigator_item_guids_from_tree(children, navigator_item_guids, navigator_item_type)
 
+def get_root_subsets(navigator_tree):
+    return navigator_tree['navigatorTree']['rootItem']['children'][0]['navigatorItem'].get('children')
+
+def get_layout(subsets,suffix = '-dwg'):
+    layout_guids = []
+    for navigator_item in subsets:
+        if navigator_item['navigatorItem']['type']=='SubsetItem' and suffix in navigator_item['navigatorItem']['name']:
+            get_navigator_item_guids_from_tree([navigator_item],layout_guids, navigator_item_type='LayoutItem')
+    return [{'navigatorItemId': guid} for guid in layout_guids]
+
+def explode_selected():
+    active_window = pyautogui.getActiveWindow()
+    if active_window:
+        active_window.activate()
+        time.sleep(1)
+        pyautogui.hotkey('ctrl', '=')
+        time.sleep(1)
+        pyautogui.hotkey('enter')
+        print(f"Сочетание отправлено в окно: {active_window.title}")
+    else:
+        print("Активное окно не найдено")
+
+def explode_drawingsonlayout(databaseId):
+    windows = aclib.RunTapirCommand(command='ChangeWindow',
+                                    parameters={'windowType': 'Layout',
+                                                'databaseId': databaseId})['success']
+    time.sleep(1)
+    if windows:
+        drawings = aclib.RunTapirCommand(command='GetElementsByType',
+                                        parameters={'elementType': 'Drawing',
+                                                    'databases': databaseId} )['elements']
+        for drawing in drawings:
+            select = aclib.RunTapirCommand ('ChangeSelectionOfElements', {'addElementsToSelection': [drawing]})['executionResultsOfAddToSelection'][0]['success']
+            if select:
+                explode_selected()
+            aclib.RunTapirCommand ('ChangeSelectionOfElements', {'addElementsToSelection': []})
+
 
 navigator_tree_id = {'navigatorTreeId': {'type': 'LayoutBook'}}
-
 navigator_tree = aclib.RunCommand(command='API.GetNavigatorItemTree',
                                   parameters=navigator_tree_id)
-
-layout_guids = []
-subsets = navigator_tree['navigatorTree']['rootItem']['children'][0]['navigatorItem'].get('children')
-
-for navigator_item in subsets:
-    if navigator_item['navigatorItem']['type']=='SubsetItem' and '-dwg' in navigator_item['navigatorItem']['name']:
-        get_navigator_item_guids_from_tree([navigator_item],layout_guids, navigator_item_type='LayoutItem')
-
-layout_elements = [{'navigatorItemId': guid} for guid in layout_guids]
+subsets = get_root_subsets(navigator_tree)
+layout_elements = get_layout(subsets, '-dwg')
 databases = aclib.RunTapirCommand(command='GetDatabaseIdFromNavigatorItemId',
                                   parameters={'navigatorItemIds': layout_elements}, debug=True)
 
 for db in databases['databases']:
-    databaseId = db['databaseId']
-    windows = aclib.RunTapirCommand(command='ChangeWindow',
-                                    parameters={'windowType': 'Layout',
-                                                'databaseId': databaseId})['success']
-    sleep(1)
-    if windows:
-        active_window = pyautogui.getActiveWindow()
-        if active_window:
-            # Активируем окно (если нужно)
-            active_window.activate()
+    explode_drawingsonlayout(db['databaseId'])
 
-            drawings = aclib.RunTapirCommand(command='GetElementsByType',
-                                            parameters={'elementType': 'Drawing',
-                                                        'databases': databaseId} )['elements']
-
-            select = aclib.RunTapirCommand ('ChangeSelectionOfElements', {'addElementsToSelection': drawings})['success']
-            if select:
-                time.sleep(2)
-                pyautogui.hotkey('ctrl', '=')
-            
-                print(f"Сочетание отправлено в окно: {active_window.title}")
-        else:
-            print("Активное окно не найдено")
 
 
 
